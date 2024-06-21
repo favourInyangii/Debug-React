71c87a0d6a428b5905b3cfbf0bc84d6cbdc10890
favour ololade <ololadefavourajayi@gmail.com>
2024-06-21 14:21:02 +0100

    fixing the app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Counter from './components/Counter';
+import Greeting from './components/Greeting';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState('John');
+
+    return (
+        <div className="App">
+            <h1>React Debugging Example</h1>
+            <Greeting name={name} />
+            <Counter />
+            <button onClick={() => setName('Jane')}>Change Name</button>
+        </div>
+    );
 }
 
 export default App;
